cab744c48dd27d33e0e45e4712c09435fda4a394
Edward J <edd.f.johnson.work@gmail.com>
2025-05-04 13:34:23 -0400

    added core and backend directories to git

---
+++ backend/src/test/java/com/ddfinance/backend/TestBackendApplication.java
@@ -0,0 +1,11 @@
+package com.ddfinance.backend;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(BackendApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
--- core/src/main/java/com/ddfinance/core/domain/UserAccount.java
+++ core/src/main/java/com/ddfinance/core/domain/UserAccount.java
@@ -1,4 +1,25 @@
 package com.ddfinance.core.domain;
 
 public class UserAccount {
+
+    String email = "";
+    String password = "";
+    String firstName = "";
+    String lastName = "";
+
+
+    public UserAccount(String email, String password, String firstName, String lastName) {
+    }
+
+    public String getEmail() {
+    }
+
+    public String getPassword() {
+    }
+
+    public String getFirstName() {
+    }
+
+    public String getLastName() {
+    }
 }
