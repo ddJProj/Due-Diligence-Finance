85ba73f8fa404de3dca2b4c3f9687ed224a26360
Edward J <edd.f.johnson.work@gmail.com>
2025-05-04 13:14:11 -0400

    implemented failing test for parameterized constructor

---
+++ core/src/test/java/com/ddfinance/core/domain/UserAccountTest.java
@@ -0,0 +1,52 @@
+package com.ddfinance.core.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserAccountTest {
+    private UserAccount userAccount;
+
+
+    @BeforeEach
+    void setUp() {
+
+//        userAccount = new UserAccount("email@test.com", "hashed-password", "firstName", "lastName", );
+        userAccount = new UserAccount();
+    }
+
+
+
+    @Test
+    void testParameterizedConstructor() {
+        String email = "email@test.com";
+        String password = "hashedPassword";
+        String firstName = "firstName";
+        String lastName = "lastName";
+
+        userAccount = new UserAccount(email, password, firstName, lastName);
+
+        assertEquals(email, userAccount.getEmail());
+        assertEquals(password, userAccount.getPassword());
+        assertEquals(firstName, userAccount.getFirstName());
+        assertEquals(lastName, userAccount.getLastName());
+
+    }
+
+
+    @Test
+    void testMethod() {
+        fail("Not implemented yet");
+    }
+
+    // TODO :  test then implement each piece of the feature
+    @Test
+    void testSetEmail() {
+        fail("Not implemented yet");
+    }
+    @AfterEach
+    void tearDown() {
+    }
+}
