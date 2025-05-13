f2bd0ce80c2dafb616d38b8f68cb84bb613cbe80
dd <edd.f.johnson.work@gmail.com>
2025-05-13 11:31:08 -0400

    Test - Added role specific enum test for each 4 values

--- core/src/test/java/com/ddfinance/core/domain/enums/RolesTest.java
+++ core/src/test/java/com/ddfinance/core/domain/enums/RolesTest.java
@@ -1,19 +1,44 @@
 package com.ddfinance.core.domain.enums;
 
+import com.ddfinance.core.domain.UserAccount;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class RolesTest {
+    private UserAccount userAccount;
+
     @BeforeEach
     void setUp() {
+        userAccount = new UserAccount();
     }
 
     @Test
-    void testMethod() {
-        fail("Not implemented yet");
+    void testClientUserAccountRole() {
+        Role client = Role.client;
+        userAccount.setRole(client);
+        assertEquals(client, userAccount.getRole());
+    }
+    @Test
+    void testGuestUserAccountRole() {
+        Role guest = Role.guest;
+        userAccount.setRole(guest);
+        assertEquals(, userAccount.getRole());
+    }
+    @Test
+    void testAdminUserAccountRole() {
+        Role admin = Role.admin;
+        userAccount.setRole(admin);
+        assertEquals(admin, userAccount.getRole());
+    }
+    @Test
+    void testEmployeeUserAccountRole() {
+        Role employee = Role.employee;
+        userAccount.setRole(employee);
+        assertEquals(employee, userAccount.getRole());
     }
 
     @AfterEach
