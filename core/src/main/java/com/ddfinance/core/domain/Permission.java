2d70bbc4d0149d58c23c53b46441797d11a06644
Edward J <edd.f.johnson.work@gmail.com>
2025-05-13 16:43:54 -0400

    Implement: Permission base entity, id value, default constructor to pass testNewEmptyPermission

--- core/src/main/java/com/ddfinance/core/domain/Permission.java
+++ core/src/main/java/com/ddfinance/core/domain/Permission.java
@@ -1,4 +1,21 @@
 package com.ddfinance.core.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Permission entity - persisted in permission table
+ */
+@Setter
+@Getter
 public class Permission {
+
+    // ID value for permission, auto generated, use lombok for access
+    private Long id;
+
+    /**
+     * Default constructor
+     */
+    public Permission() {}
+
 }
