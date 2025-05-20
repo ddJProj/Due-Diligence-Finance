725b8c17170e2b08e702673a440df921d164391b
Edward J <edd.f.johnson.work@gmail.com>
2025-05-20 19:20:02 -0400

    Implement: added parameterized constructor to Client.java - paramemterized constructor that can be used to create a non-default Client entity instance. included the attribute definitions (and their relevant annotations) that are necessary for basic usage

--- core/src/main/java/com/ddfinance/core/domain/Client.java
+++ core/src/main/java/com/ddfinance/core/domain/Client.java
@@ -1,4 +1,42 @@
 package com.ddfinance.core.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "client")
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String clientId;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", unique = true)
+    private UserAccount userAccount;
+
+    @ManyToOne
+    @JoinColumn(name = "assigned_employee_id")
+    private Employee assignedEmployeePartner;
+
+    // FIXME: Implement a better way to generate clientId (ie, "userAccount.id" + "-" + "client.id")
+    public Client(String clientId, UserAccount userAccount, Employee assignedEmployeePartner) {
+        this.clientId = clientId;
+        setUserAccount(userAccount);
+        setAssignedEmployeePartner(assignedEmployeePartner);
+    }
+
+
+
+
+
+//    private Employee assignedEmployeePair;
+
 }
