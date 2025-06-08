cd12afe943923ec4881c1a16c42894e7b43b7387
Edward J <edd.f.johnson.work@gmail.com>
2025-06-08 10:49:58 -0400

    Reorganized the permissions enum to match class files. Added new permission to the Client level permissions, "MESSAGE_PARTNER"

--- core/src/main/java/com/ddfinance/core/domain/enums/Permissions.java
+++ core/src/main/java/com/ddfinance/core/domain/enums/Permissions.java
@@ -8,27 +8,27 @@ import java.util.Set;
 @Getter
 public enum Permissions {
 
-    /**
-     * Client level permissions
-     */
+
+
     /**
      * UserAccount level:
      */
     VIEW_ACCOUNT("View the details of you UserAccount."),
-    VIEW_ACCOUNTS("View all UserAccounts and details."),
     EDIT_MY_DETAILS("Edits the account details of this specific UserAccount instance."),
     UPDATE_MY_PASSWORD("Updates the stored password for this UserAccount's own password."),
     CREATE_USER("Creates a new UserAccount."),
 
     /**
-     * Admin level:
+     * Guest level:
      */
-    EDIT_USER("Edits the details of a specific UserAccount."),
-    DELETE_USER("Removes a UserAccount from the system."),
-    EDIT_EMPLOYEE("Edit the details of a specific Employee account instance."),
-    CREATE_EMPLOYEE("Creates a new Employee account instance."),
-    UPDATE_OTHER_PASSWORD("Updates the stored password for the target UserAccount."),
+    REQUEST_CLIENT_ACCOUNT("Request upgrade to Client account status with the firm."),
 
+
+    /**
+     * Client level:
+     */
+    VIEW_INVESTMENT("Views the details of a specific investment for this Client account instance."),
+    MESSAGE_PARTNER("Direct message to the Employee that a Client is partnered with at the firm. Used to modify investments, etc."),
 
 
     /**
@@ -38,24 +38,23 @@ public enum Permissions {
     EDIT_CLIENT("Edits the details of an existing Client account."),
     VIEW_CLIENT("Views the details of a specific client account instance"),
     VIEW_CLIENTS("Lists the details of all Client account instances"),
-    ASSIGN_CLIENT("Assigns an individual Client account instance to an Employeee partner."),
+    ASSIGN_CLIENT("Assigns an individual Client account instance to an Employee partner."),
     CREATE_INVESTMENT("Creates a new investment for a specific Client account instance."),
     EDIT_INVESTMENT("Edits an existing investment for a specific Client account instance."),
     VIEW_EMPLOYEES("Lists the details of all Employee account instances."),
     VIEW_EMPLOYEE("Lists the details of a specific Employee account instance."),
 
-
-
     /**
-     * Client level:
+     * Admin level:
      */
-    VIEW_INVESTMENT("Views the details of a specific investment for this Client account instance."),
+    EDIT_USER("Edits the details of a specific UserAccount."),
+    DELETE_USER("Removes a UserAccount from the system."),
+    EDIT_EMPLOYEE("Edit the details of a specific Employee account instance."),
+    CREATE_EMPLOYEE("Creates a new Employee account instance."),
+    UPDATE_OTHER_PASSWORD("Updates the stored password for the target UserAccount."),
+    VIEW_ACCOUNTS("View all UserAccounts and details.");
 
 
-    /**
-     * Guest level:
-     */
-    REQUEST_CLIENT_ACCOUNT("Request upgrade to Client account status with the firm.");
 
     private final String description;
 
