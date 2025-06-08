93d3484cdd5309592316bc36b459339143f6c5d2
Edward J <edd.f.johnson.work@gmail.com>
2025-06-08 09:21:59 -0400

    Implement: define core Employee class - define generate and get EmployeeId methods, default constructor, parameterized constructor, and initial annotations for class & attributes

--- core/src/main/java/com/ddfinance/core/domain/Employee.java
+++ core/src/main/java/com/ddfinance/core/domain/Employee.java
@@ -1,4 +1,75 @@
 package com.ddfinance.core.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "employee")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true)
+    private String employeeId;
+
+    @OneToOne
+    @JoinColumn(name = "user_id" ,unique = true)
+    private UserAccount userAccount;
+
+    // FIXME: FIX THIS? do we need use the id or employee string
+    @OneToMany(mappedBy = "assignedEmployeePartner", fetch = FetchType.LAZY)
+    private Set<Client> clientList = new HashSet<>();
+
+    private String locationId;
+    private String jobTitle;
+
+    public Employee(){
+        this.locationId = "USA";
+        this.clientList = new HashSet<>();
+    }
+
+    public Employee(String employeeId, UserAccount userAccount) {
+        this();
+        setUserAccount(userAccount);
+        setJobTitle(jobTitle);
+    }
+
+
+    /**
+     *
+     * @return
+     */
+    public String getEmployeeId() {
+        if (employeeId == null) {
+            generateEmployeeId();
+        }
+        return employeeId;
+    }
+
+    /**
+     *
+     */
+    private void generateEmployeeId(){
+        if (userAccount != null && userAccount.getId() != null && id != null) {
+            this.employeeId= userAccount.getId() + "_" + id;
+        }
+    }
+
+    /**
+     * Persist the generated values
+     */
+    @PostPersist
+    private void postPersist(){
+        generateEmployeeId();
+    }
+
+
 }
