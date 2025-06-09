129c9372dd87c96e682bcdde4a38cb93254af6a2
Edward J <edd.f.johnson.work@gmail.com>
2025-06-09 11:25:37 -0400

    Implement: DatabaseException + child exception classes implemented to pass tests - constructor with message & message+throwable. Child class exceptions : testConnectionException, ,testNotFoundException, testQueryException, testTransactionException

--- core/src/main/java/com/ddfinance/core/exception/DatabaseException.java
+++ core/src/main/java/com/ddfinance/core/exception/DatabaseException.java
@@ -1,4 +1,40 @@
 package com.ddfinance.core.exception;
 
-public class DatabaseException {
+public class DatabaseException extends ApplicationException {
+    public DatabaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DatabaseException(String message) {
+        super(message);
+    }
+
+    public static class ConnectionException extends DatabaseException{
+        public ConnectionException(String message){
+            super(message);
+        }
+    }
+
+
+    public static class QueryException extends DatabaseException{
+        public QueryException(String message){
+            super(message);
+        }
+    }
+
+
+    public static class TransactionException extends DatabaseException{
+        public TransactionException(String message){
+            super(message);
+        }
+    }
+
+
+    public static class NotFoundException extends DatabaseException{
+        public NotFoundException(String message){
+            super(message);
+        }
+    }
+
+
 }
