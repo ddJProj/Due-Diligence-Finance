598cb2fc4bb6443b43d071d6ffa2b2bde970557a
Edward J <edd.f.johnson.work@gmail.com>
2025-06-09 11:07:55 -0400

    Implement: ApplicationException + child exception classes implemented to pass tests - constructor with message & message+throwable. Child class exceptions : PasswordHashException, ,testPasswordHashException, testPasswordValidationException, testEnvironmentVariableException,testInputException, testConfigurationException, testNoMatchException

--- core/src/main/java/com/ddfinance/core/exception/ApplicationException.java
+++ core/src/main/java/com/ddfinance/core/exception/ApplicationException.java
@@ -1,4 +1,45 @@
 package com.ddfinance.core.exception;
 
-public class ApplicationException {
+public class ApplicationException extends Exception {
+
+    public ApplicationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ApplicationException(String message) {
+        super(message);
+    }
+
+    public static class PasswordHashException extends ApplicationException {
+        public PasswordHashException(String message) {
+            super(message);
+        }
+    }
+    public static class PasswordValidationException extends ApplicationException{
+        public PasswordValidationException(String message){
+            super(message);
+        }
+    }
+    public static class EnvironmentVariableException extends ApplicationException{
+        public EnvironmentVariableException(String message){
+            super(message);
+        }
+    }
+    public static class InputException extends ApplicationException{
+        public InputException(String message){
+            super(message);
+        }
+    }
+    public static class ConfigurationException extends ApplicationException{
+        public ConfigurationException(String message){
+            super(message);
+        }
+    }
+
+    public static class NoMatchException extends ApplicationException{
+        public NoMatchException(String message){
+            super(message);
+        }
+    }
+
 }
