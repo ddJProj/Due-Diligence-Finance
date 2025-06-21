5b2ee98fd466d57525a152f1a32e066ad994918c
Edward J <edd.f.johnson.work@gmail.com>
2025-06-20 21:37:53 -0400

    Test: initial test file for UserAccountRepository added

---
+++ core/src/test/java/com/ddfinance/core/repository/UserAccountRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.ddfinance.core.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserAccountRepositoryTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testMethod() {
+        fail("Not implemented yet");
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
