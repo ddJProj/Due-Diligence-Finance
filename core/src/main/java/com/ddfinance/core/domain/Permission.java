0121d05d24ecd7ea547343581d9089abb187ac00
Edward J <edd.f.johnson.work@gmail.com>
2025-07-05 12:17:28 -0400

    Impl-Permission: finished Permission entity with JPA annotations and all current required methods

--- core/src/main/java/com/ddfinance/core/domain/Permission.java
+++ core/src/main/java/com/ddfinance/core/domain/Permission.java
@@ -5,6 +5,10 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 /**
  * Permission entity - persisted in permission table
@@ -13,8 +17,9 @@ import lombok.Setter;
 @Getter
 @Entity
 // use table "permission"
-@Table(name = "permission")
+@Table(name = "permissions")
 @NoArgsConstructor
+@ToString
 public class Permission {
 
     // ID value for permission, auto generated, use lombok for access
@@ -31,6 +36,8 @@ public class Permission {
 
     private String description;
 
+//    @ManyToMany(mappedBy = "permissions")
+//    private Set<UserAccount> userAccounts = new HashSet<>();
 
 
     /**
@@ -41,4 +48,19 @@ public class Permission {
         this.description = permissionDescription;
     }
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Permission that = (Permission) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
