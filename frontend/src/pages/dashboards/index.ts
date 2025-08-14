0cc3141321653dab8e2f707018278aaa28140db4
Edward J <edd.f.johnson.work@gmail.com>
2025-08-14 12:39:26 -0400

    impl: Export AdminDashboard from dashboards index - Add AdminDashboard to dashboard exports - Configure module exports for routing

--- frontend/src/pages/dashboards/index.ts
+++ frontend/src/pages/dashboards/index.ts
@@ -1,6 +1,7 @@
 // frontend/src/pages/dashboards/index.ts
 
-export { ClientDashboard } from './ClientDashboard';
-export { EmployeeDashboard } from './EmployeeDashboard';
+export { default as ClientDashboard } from './ClientDashboard';
+export { default as EmployeeDashboard } from './EmployeeDashboard';
+export { default as AdminDashboard } from './AdminDashboard';
 
 
