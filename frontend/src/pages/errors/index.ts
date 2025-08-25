a296a896ab0f4b29a669d3a0c839ba4e0cff8cbb
Edward J <edd.f.johnson.work@gmail.com>
2025-08-25 15:28:26 -0400

    impl: Export error pages from pages/errors - Export NotFoundPage component - Export ForbiddenPage component - Export ServerErrorPage component

---
+++ frontend/src/pages/errors/index.ts
@@ -0,0 +1,5 @@
+// frontend/src/pages/errors/index.ts
+
+export { NotFoundPage } from './NotFoundPage';
+export { ForbiddenPage } from './ForbiddenPage';
+export { ServerErrorPage } from './ServerErrorPage';
