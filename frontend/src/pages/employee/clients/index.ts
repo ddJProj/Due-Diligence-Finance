c30937ae6e548755700d0eb7c5b6a49bcdfaa1a1
Edward J <edd.f.johnson.work@gmail.com>
2025-08-25 16:19:44 -0400

    impl: Export ClientListPage from employee clients index - Export ClientListPage component - Prepare for ClientDetailsPage export

---
+++ frontend/src/pages/employee/clients/index.ts
@@ -0,0 +1,3 @@
+// frontend/src/pages/employee/clients/index.ts
+export { default as ClientListPage } from './ClientListPage';
+// export { default as ClientDetailsPage } from './ClientDetailsPage';
