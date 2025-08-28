83b468d40bd3bd1373e68450c1fe6ea0bffda09d
Edward J <edd.f.johnson.work@gmail.com>
2025-08-28 15:45:52 -0400

    impl: Export UserListPage from admin users index - Export UserListPage component - Prepare for CreateUserPage and EditUserPage exports

---
+++ frontend/src/pages/admin/users/index.ts
@@ -0,0 +1,5 @@
+// frontend/src/pages/admin/users/index.ts
+export { default as UserListPage } from './UserListPage';
+// implement next:
+// export { default as CreateUserPage } from './CreateUserPage';
+// export { default as EditUserPage } from './EditUserPage';
