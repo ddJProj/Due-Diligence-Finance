3f8bb16b9d9a79c9b3da8c4102b5182750a902f0
Edward J <edd.f.johnson.work@gmail.com>
2025-09-10 10:05:47 -0400

    impl: Export ContactPage from public pages - Add ContactPage to public index - Update module exports

--- frontend/src/pages/public/index.ts
+++ frontend/src/pages/public/index.ts
@@ -2,4 +2,13 @@
 
 export { default as LandingPage } from './LandingPage';
 export { default as AboutPage } from './AboutPage';
+export { default as ContactPage } from './ContactPage';
+
+
+
+
+
+
+
+
 
