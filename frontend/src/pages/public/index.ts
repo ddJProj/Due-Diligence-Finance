f1b941e237a991aa707f95ffd2f3268a0a203fe8
Edward J <edd.f.johnson.work@gmail.com>
2025-09-10 10:11:46 -0400

    impl: Export PricingPage from public pages - Add PricingPage to public index - Complete public pages exports

--- frontend/src/pages/public/index.ts
+++ frontend/src/pages/public/index.ts
@@ -3,9 +3,8 @@
 export { default as LandingPage } from './LandingPage';
 export { default as AboutPage } from './AboutPage';
 export { default as ContactPage } from './ContactPage';
-
 export { default as FeaturesPage } from './FeaturesPage';
-
+export { default as PricingPage } from './PricingPage';
 
 
 
